c91e3556954d38e314dfe43dcabe508179c2fe2e
ali-norouzian <ali.norouzian@outlook.com>
2023-12-14 20:34:06 +0330

    Generic repo added

--- src/Services/Product/Product.Domain/Common/EntityBase.cs
+++ src/Services/Product/Product.Domain/Common/EntityBase.cs
@@ -2,9 +2,9 @@
 
 namespace Product.Domain.Common
 {
-	public abstract class EntityBase
-	{
-		[Key]
-		public long Id { get; protected set; }
-	}
+    public abstract class EntityBase
+    {
+        [Key]
+        public long Id { get; protected set; }
+    }
 }
--- src/Services/Product/Product.Domain/Entities/Product.cs
+++ src/Services/Product/Product.Domain/Entities/Product.cs
@@ -3,21 +3,21 @@ using Product.Domain.Common;
 
 namespace Product.Domain.Entities
 {
-	public class Product : EntityBase
-	{
-		[MaxLength(100)]
-		public string Name { get; set; }
+    public class Product : EntityBase
+    {
+        [MaxLength(100)]
+        public string Name { get; set; }
 
-		public DateTime ProduceDate { get; set; }
+        public DateTime ProduceDate { get; set; }
 
-		[MaxLength(20)]
-		public string ManufacturePhone { get; set; }
+        [MaxLength(20)]
+        public string ManufacturePhone { get; set; }
 
-		[MaxLength(100)]
-		public string ManufactureEmail { get; set; }
+        [MaxLength(100)]
+        public string ManufactureEmail { get; set; }
 
-		public bool IsAvailable { get; set; }
-	}
+        public bool IsAvailable { get; set; }
+    }
 }
 
 // Product { Name , *ProduceDate , ManufacturePhone , *ManufactureEmail , IsAvailable }
